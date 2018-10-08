c66801e354a1278f2808f02d1ae4583151cbca8d
Fabien Lasserre <fabien@primative.net>
2018-10-08 12:28:43 +0200

    :wrench: Update how webpack behaves

--- webpack.common.js
+++ webpack.common.js
@@ -1,85 +1,26 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const GoogleFontsPlugin = require("google-fonts-webpack-plugin");
 
 module.exports = {
-    performance: {
-        hints: false
-    },
+    // performance: {
+    //     hints: false
+    // },
     entry: './assets/js/main.js',
     output: {
-        filename: 'js/main.js',
+        filename: 'js/main.min.js',
         path: path.resolve(__dirname, 'static')
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.scss$/,
-                exclude: /(node_modules|bower_components)/,
-                // loader: ExtractTextPlugin.extract(['css-loader?url=false', 'sass-loader'])
-                use: [
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false
-                        }
-                    },
-                    {
-                        loader: 'resolve-url-loader'
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                            sourceMapContents: false
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                    // you can specify a publicPath here
-                    // by default it use publicPath in webpackOptions.output
-                    publicPath: 'public/css'
-                    }
-                },
-                "css-loader"
-                ]
-            }
-        ]
-    },
-    plugins: [
-        // new GoogleFontsPlugin({
-        //     fonts: [
-        //         { family: "Montserrat", variants: [ "300", "400", "400italic", "700", "700italic" ] },
-        //         { family: "Open Sans", variants: [ "400", "400italic", "700", "700italic" ] },
-        //         { family: "Ubuntu Mono", variants: [ "400", "400italic" ] }
-        //     ]
-        // }),
-        // new ExtractTextPlugin({
-        //     filename: "css/main.css",
-        //     allChunks: true,
-        //     disable: process.env.NODE_ENV === "development"
-        // })
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: "main.css",
-            // chunkFilename: "[id].css"
-          })
-    ]
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.js$/,
+    //             exclude: /(node_modules|bower_components)/,
+    //             use: {
+    //                 loader: 'babel-loader',
+    //                 options: {
+    //                     presets: ['@babel/preset-env']
+    //                 }
+    //             }
+    //         },
+    //     ]
+    // },
 };
