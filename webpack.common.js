6c39ee7d00caa7d061abcb6c49b67f6b58186c45
Fabien Lasserre <fabien@primative.net>
2020-04-23 11:34:19 +0200

    :art: Add resource fingerprints

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    // performance: {
-    //     hints: false
-    // },
-    entry: './assets/js/main.js',
-    output: {
-        filename: 'js/main.min.js',
-        path: path.resolve(__dirname, 'static')
-    },
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.js$/,
-    //             exclude: /(node_modules|bower_components)/,
-    //             use: {
-    //                 loader: 'babel-loader',
-    //                 options: {
-    //                     presets: ['@babel/preset-env']
-    //                 }
-    //             }
-    //         },
-    //     ]
-    // },
+  entry: './assets/js/main.js',
+  output: {
+    filename: 'js/main.min.js',
+    path: path.resolve(__dirname, 'assets'),
+  },
 };
